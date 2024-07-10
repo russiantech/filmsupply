1b33eb5446af9a1f65e7576d91d224cd8463425c
russiantech <chrisjsmes@gmail.com>
2024-07-10 11:51:58 +0100

    after deleting the former

--- web/confiq.py
+++ web/confiq.py
@@ -19,12 +19,12 @@ SQLALCHEMY_DATABASE_ENGINE = {
     'rollback_on_exception': True,
 }
 
-# SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URI')
+SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URI')
 
-SQLALCHEMY_DATABASE_URI = (
+""" SQLALCHEMY_DATABASE_URI = (
         f"mysql+mysqlconnector://{os.getenv('MYSQL_USER')}:{os.getenv('MYSQL_PASSWORD')}"
         f"@{os.getenv('MYSQL_HOST')}:{os.getenv('MYSQL_PORT')}/{os.getenv('MYSQL_DATABASE')}"
-    )   
+    )  """  
  
 SQLALCHEMY_TRACK_MODIFICATIONS = False
 SQLALCHEMY_POOL_SIZE = 50   # Increase the pool size if necessary
